03ad7a5e1c3a736d4e3f71152610b480897ec826
Tyler Schuett <47.anomaly@gmail.com>
2014-02-20 18:10:14 -0800

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matchbox::Application.config.secret_token = '020f3033826ef8632dfd47c6b2640994b5a0d2db424e458fa8af80b1c60cf3f6830f28f2590a90641554f6a62d121b13a6b0f1f252b6abb3f50f7f75667a8d91'
